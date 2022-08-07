87ab9daae8d8fa81282e8615786fc5f2ee9ee5b3
Erika Freitas <erikamsfreitas@gmail.com>
2022-08-07 20:54:26 -0300

    Add presentation

--- app/src/main/java/com/ef/githubrepos/App.kt
+++ app/src/main/java/com/ef/githubrepos/App.kt
@@ -2,6 +2,8 @@ package com.ef.githubrepos
 
 import android.app.Application
 import com.ef.githubrepos.data.di.DataModule
+import com.ef.githubrepos.domain.di.DomainModule
+import com.ef.githubrepos.presentation.di.PresentationModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -15,5 +17,7 @@ class App : Application() {
         }
 
         DataModule.load()
+        DomainModule.load()
+        PresentationModule.load()
     }
 }
---
+++ app/src/main/java/com/ef/githubrepos/presentation/di/PresentationModule.kt
@@ -0,0 +1,21 @@
+package com.ef.githubrepos.presentation.di
+
+import com.ef.githubrepos.domain.ListUserReposUseCase
+import com.ef.githubrepos.presentation.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.loadKoinModules
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+object PresentationModule {
+
+    fun load() {
+        loadKoinModules(viewModelModule())
+    }
+
+    private fun viewModelModule(): Module {
+        return module {
+            viewModel { MainViewModel(get()) }
+        }
+    }
+}
